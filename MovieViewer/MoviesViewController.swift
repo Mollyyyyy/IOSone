6d8e30f4068adf06f0dbc043c9b342d63c0c505c
Mollyyyyy <apple@appletekiair-4.dynamic.ucsd.edu>
2017-02-05 17:46:49 -0800

    have problem in search bar

--- MovieViewer/MoviesViewController.swift
+++ MovieViewer/MoviesViewController.swift
@@ -10,31 +10,33 @@ import UIKit
 import AFNetworking
 import MBProgressHUD
 
-class MoviesViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+class MoviesViewController: UIViewController,UITableViewDataSource,UITableViewDelegate,UISearchBarDelegate {
 
 @IBOutlet weak var tableView: UITableView!
+@IBOutlet weak var searchBar: UISearchBar!
 
 var movies: [NSDictionary]?
 
+
 override func viewDidLoad() {
-    super.viewDidLoad()
+super.viewDidLoad()
+
     tableView.dataSource = self
     tableView.delegate = self
-    
+    searchBar.delegate = self
     let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
     let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")!
     let myRequest = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
     let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
     let task: URLSessionDataTask = session.dataTask(with: myRequest) { (data: Data?, response: URLResponse?, error: Error?) in
-
-    if let data = data {
-        if let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
-            print(dataDictionary)
-            
-            self.movies = dataDictionary["results"] as? [NSDictionary]
-            self.tableView.reloadData()
+        if let data = data {
+            if let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
+                print(dataDictionary)
+                self.movies = dataDictionary["results"] as? [NSDictionary]
+                self.tableView.reloadData()
+                
+            }
         }
-    }
     };
     self.loadDataFromNetwork()
     // Initialize a UIRefreshControl
@@ -42,77 +44,103 @@ override func viewDidLoad() {
     refreshControl.addTarget(self, action: #selector(refreshControlAction(_:)), for:UIControlEvents.valueChanged)
     self.tableView.insertSubview(refreshControl, at: 0)
     task.resume()
-    
+
 }
+    
+    
+    
 // Makes a network request to get updated data
 // Updates the tableView with the new data
 // Hides the RefreshControlsel
 func refreshControlAction(_ refreshControl: UIRefreshControl) {
-    
+
     // ... Create the URLRequest `myRequest` ...
-
-let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
-let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")!
-let myRequest = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
-    // Configure session so that completion handler is executed on main UI thread
-let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
-let task: URLSessionDataTask = session.dataTask(with: myRequest) { (data: Data?, response: URLResponse?, error: Error?) in
-    
-        // ... Use the new data to update the data source ...
-    if let data = data {
-        if let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
-            print(dataDictionary)
-            
-            self.movies = dataDictionary["results"] as? [NSDictionary]
-            self.tableView.reloadData()
-        }
-    }
-    };
-
-        // Reload the tableView now that there is new data
-        self.tableView.reloadData()
-        
-        // Tell the refreshControl to stop spinning
-        refreshControl.endRefreshing()
-
-    task.resume()
-}
-func loadDataFromNetwork() {
-
-    // ... Create the NSURLRequest (myRequest) ...
-
     let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
     let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")!
     let myRequest = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+    // Configure session so that completion handler is executed on main UI thread
+    let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+    let task: URLSessionDataTask = session.dataTask(with: myRequest) { (data: Data?, response: URLResponse?, error: Error?) in
+        // ... Use the new data to update the data source ...
+        if let data = data {
+            if let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
+                print(dataDictionary)
+                
+                self.movies = dataDictionary["results"] as? [NSDictionary]
+                self.tableView.reloadData()
+            }
+        }
+    };
+    // Reload the tableView now that there is new data
+    self.tableView.reloadData()
+    // Tell the refreshControl to stop spinning
+    refreshControl.endRefreshing()
+    task.resume()
+}
+    
+    
+    
+    
+func loadDataFromNetwork() {
 
+    // ... Create the NSURLRequest (myRequest) ...
+    let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+    let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")!
+    let myRequest = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
     // Configure session so that completion handler is executed on main UI thread
     let session = URLSession(
-    configuration: URLSessionConfiguration.default,
-    delegate:nil,
-    delegateQueue:OperationQueue.main
+        configuration: URLSessionConfiguration.default,
+        delegate:nil,
+        delegateQueue:OperationQueue.main
     )
-
     // Display HUD right before the request is made
     MBProgressHUD.showAdded(to: self.view, animated: true)
-
     let task : URLSessionDataTask = session.dataTask(with: myRequest,
     completionHandler: { (data, response, error) in
-
     // Hide HUD once the network request comes back (must be done on main UI thread)
     MBProgressHUD.hide(for: self.view, animated: true)
-
-
     });
     task.resume()
 }
-
-override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+    
+    
+    
+// This method updates filteredData based on the text in the Search Box
+func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+    // When there is no text, filteredData is the same as the original data
+    // When user has entered text into the search box
+    // Use the filter method to iterate over all items in the data array
+    // For each item, return true if the item should be included and false if the
+    // item should NOT be included
+    let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+    let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")!
+    let myRequest = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+    // Configure session so that completion handler is executed on main UI thread
+    let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task: URLSessionDataTask = session.dataTask(with: myRequest) { (data: Data?, response: URLResponse?, error: Error?) in
+            // ... Use the new data to update the data source ...
+         if let data = data {
+            if let dataDictionary = try! JSONSerialization.jsonObject(with: data, options:[]) as? NSDictionary {
+                print(dataDictionary)
+                self.movies = searchText.isEmpty ? data : data.filter({(dataString : String) -> Bool in
+                    // If dataItem matches the searchText, return true to include it
+                    return dataString.range(of: searchText,options: .caseInsensitive) != nil
+                    //return dataString.range(of: searchText, options: .caseInsensitive) != nil
+                })
+                self.tableView.reloadData()
+            }
+        }
+    };
+  task.resume()
 }
     
     
-    
+override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+// Dispose of any resources that can be recreated.
+}
+
+
 func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
     if let movies = movies{
         return movies.count
@@ -123,9 +151,8 @@ func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> 
 }
 
 func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
-    
+
     let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
-    
     let movie = movies![indexPath.row]
     let title = movie["title"] as! String
     let overview = movie["overview"] as! String
@@ -138,13 +165,14 @@ func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> U
     print("row\(indexPath.row)")
     return cell
 }
+
 /*
 // MARK: - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-    // Get the new view controller using segue.destinationViewController.
-    // Pass the selected object to the new view controller.
+// Get the new view controller using segue.destinationViewController.
+// Pass the selected object to the new view controller.
 }
 */
 
